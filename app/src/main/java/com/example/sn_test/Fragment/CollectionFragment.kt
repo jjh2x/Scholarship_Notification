cdead529be02bf330f094ec28049a08a7c36108f
jjh2x <68693224+jjh2x@users.noreply.github.com>
2021-02-02 22:38:36 +0900

    홈 - calendarview 적용. 컬렉션뷰 - recyclerview 적용.

--- app/src/main/java/com/example/sn_test/Fragment/CollectionFragment.kt
+++ app/src/main/java/com/example/sn_test/Fragment/CollectionFragment.kt
@@ -5,14 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.sn_test.R
 
 class CollectionFragment: Fragment() {
+    /* 장학금 요약 클래스를 초기화한 배열 */
+    var summaryList = arrayListOf<ScholarshipSummary> (
+        ScholarshipSummary("장학금1", "날짜1", "금액1"),
+        ScholarshipSummary("장학금2", "날짜2", "금액2"),
+        ScholarshipSummary("장학금3", "날짜3", "금액3"),
+        ScholarshipSummary("장학금4", "날짜4", "금액4"),
+        ScholarshipSummary("장학금5", "날짜5", "금액5"),
+        ScholarshipSummary("장학금6", "날짜6", "금액6"),
+        ScholarshipSummary("장학금7", "날짜7", "금액7"),
+        ScholarshipSummary("장학금8", "날짜8", "금액8"),
+        ScholarshipSummary("장학금9", "날짜9", "금액9"),
+        ScholarshipSummary("장학금10", "날짜10", "금액10"),
+        ScholarshipSummary("장학금11", "날짜11", "금액11")
+    )
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_collection, container, false)
+        var rootView = inflater.inflate(R.layout.fragment_collection, container, false)
+        recyclerView = rootView.findViewById(R.id.recyclerView!!) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val mAdapter = MainRvAdapter(requireContext(), summaryList)
+        recyclerView.adapter = mAdapter
+        recyclerView.setHasFixedSize(true)
+        return rootView
     }
 }
--- app/src/main/java/com/example/sn_test/MainActivity.kt
+++ app/src/main/java/com/example/sn_test/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.sn_test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.viewpager.widget.PagerAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import com.example.sn_test.Adpater.MainFragmentStatePAgerAdapter
+import com.example.sn_test.Fragment.MainRvAdapter
+import com.example.sn_test.Fragment.ScholarshipSummary
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
-    lateinit var vp_ac_main_frag_pager : ViewPager
-    lateinit var tl_ac_main_bottom_menu : TabLayout
+    lateinit var vp_ac_main_frag_pager: ViewPager
+    lateinit var tl_ac_main_bottom_menu: TabLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,10 +29,15 @@ class MainActivity : AppCompatActivity() {
         tl_ac_main_bottom_menu = findViewById<TabLayout>(R.id.tl_ac_main_bottom_menu)
         tl_ac_main_bottom_menu.setupWithViewPager(vp_ac_main_frag_pager)
 
-        val bottomNaviLayout : View = this.layoutInflater.inflate(R.layout.bottom_navigation_tab, null, false)
-        tl_ac_main_bottom_menu.getTabAt(0)!!.customView = bottomNaviLayout.findViewById(R.id.btn_bottom_navi_home)
-        tl_ac_main_bottom_menu.getTabAt(1)!!.customView = bottomNaviLayout.findViewById(R.id.btn_bottom_navi_collection)
-        tl_ac_main_bottom_menu.getTabAt(2)!!.customView = bottomNaviLayout.findViewById(R.id.btn_bottom_navi_search)
-        tl_ac_main_bottom_menu.getTabAt(3)!!.customView = bottomNaviLayout.findViewById(R.id.btn_bottom_navi_myPage)
+        val bottomNaviLayout: View =
+            this.layoutInflater.inflate(R.layout.bottom_navigation_tab, null, false)
+        tl_ac_main_bottom_menu.getTabAt(0)!!.customView =
+            bottomNaviLayout.findViewById(R.id.btn_bottom_navi_home)
+        tl_ac_main_bottom_menu.getTabAt(1)!!.customView =
+            bottomNaviLayout.findViewById(R.id.btn_bottom_navi_collection)
+        tl_ac_main_bottom_menu.getTabAt(2)!!.customView =
+            bottomNaviLayout.findViewById(R.id.btn_bottom_navi_search)
+        tl_ac_main_bottom_menu.getTabAt(3)!!.customView =
+            bottomNaviLayout.findViewById(R.id.btn_bottom_navi_myPage)
     }
 }
